5f56162dcd6918bfd8c01d8eb5eb61f84ef5f5c2
DausHamyR <daus.amat354@gmail.com>
2023-05-03 20:50:03 +0700

    repair

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,41 @@ import { useState } from 'react';
 import Counter from './components/Counter';
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [errorIncrement, setErrorIncrement] = useState(false);
-    const [errorDecrement, setErrorDecrement] = useState(false);
+  const [count, setCount] = useState(0);
+  const [errorIncrement, setErrorIncrement] = useState("");
+  const [errorDecrement, setErrorDecrement] = useState("");
 
-    const handleIncrement = () => {
-        if (count < 10) {
-            setCount(count + 1);
-            setErrorIncrement(false);
-        } else {
-        setErrorIncrement(true);
-        }
-    };
+  const handleIncrement = () => {
+    if (count >= 10) {
+      setErrorIncrement("Maks 10");
+      setErrorDecrement("");
+      return;
+    }
 
-    const handleDecrement = () => {
-        if (count > 0) {
-            setCount(count - 1);
-            setErrorDecrement(false);
-        } else {
-        setErrorDecrement(true);
-        }
-    };
+    setCount(count + 1);
+    setErrorIncrement("");
+    setErrorDecrement("");
+  };
 
-    return (
-    <div className="container mx-auto h-[100vh] flex justify-center align-center">
-        <Counter count={count} error={error} handleIncrement={handleIncrement} handleDecrement={handleDecrement} />
+  const handleDecrement = () => {
+    if (count <= 0) {
+      setErrorDecrement("Failed");
+      setErrorIncrement("");
+      return;
+    }
+
+    setCount(count - 1);
+    setErrorDecrement("");
+    setErrorIncrement("");
+  };
+
+  return (
+    <div className="container mx-auto h-[70vh] grid justify-items-center content-center">
+      <h1 className='text-red-500 mt-2'>{errorIncrement}</h1>
+      <h1 className='text-red-500 mt-2'>{errorDecrement}</h1>
+      <Counter count={count} handleIncrement={handleIncrement} handleDecrement={handleDecrement} />
     </div>
-    );
+  );
 }
 
 export default App;
